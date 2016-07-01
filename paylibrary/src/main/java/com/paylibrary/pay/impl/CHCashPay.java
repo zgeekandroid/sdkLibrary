eac7505af50b98ecb9e6d307e61092ae483ba5e5
chengbing1212 <479043268@qq.com>
2016-07-01 14:35:26 +0800

    新建一个sdklibrary

---
+++ paylibrary/src/main/java/com/paylibrary/pay/impl/CHCashPay.java
@@ -0,0 +1,23 @@
+package com.paylibrary.pay.impl;
+
+import com.commonslibrary.commons.net.RequestCallBack;
+import com.paylibrary.pay.IPay;
+
+import java.util.Map;
+
+
+public abstract class CHCashPay extends IPay {
+    @Override
+    public void getPayParam(Map<String, Object> params, RequestCallBack<String> callBack) {
+        throw  new IllegalStateException("不需要调用此方法");
+    }
+    @Override
+    public void getPayResult(Map<String, Object> params, RequestCallBack<String> callBack) {
+        throw  new IllegalStateException("不需要调用此方法");
+    }
+
+    @Override
+    public void requestOrder(Map<String, Object> params, RequestCallBack<String> callBack) {
+
+    }
+}
---
+++ paylibrary/src/main/java/com/paylibrary/pay/impl/PayTypeEnum.java
@@ -0,0 +1,10 @@
+package com.paylibrary.pay.impl;
+
+/**
+ * date        :  2016-04-02  17:45
+ * author      :  Mickaecle gizthon
+ * description :
+ */
+public enum PayTypeEnum {
+    ALIPAY,WEIXINPAY,YUEPAY,CASHPAY;
+}
